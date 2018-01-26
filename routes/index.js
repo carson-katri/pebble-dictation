927ba4e20e9bbe77b67d3a15657415f79efddd85
Carson Katri <Carson.katri@gmail.com>
2018-01-26 17:55:57 -0500

    Write to audio file for now

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var Nuance = require('nuance');
+
 var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,8 +16,24 @@ router.get('/boot', function(req, res, next) {
 });
 
 /* PROXY */
-router.post('/request', function(req, res, next) {
-	
+router.post('/NmspServlet/*', function(req, res, next) {
+	/*
+	nuance.sendDictationRequest({
+	    "identifier": "randomIdentifierStringHere",
+	    "language": "en-US",
+	    "path": "audio.wav",
+	    "additionalHeaders": {}, //If you'd like to supply more headers or replace the default headers, supply them here. 
+	    "success": function(resp){ //The success callback function. 
+	        console.log(resp);
+	    },
+	    "error": function(resp){ //The error callback function - returns the response from Nuance that you can debug. 
+	        console.log("An error was occurred.");
+	        console.log(resp);
+	    }
+	});
+	*/
+	var writeStream = fs.createWriteStream('../public/audio.wav');
+	req.pipe(writeStream);
 });
 
 module.exports = router;
