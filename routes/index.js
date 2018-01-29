fb05fd6d89bb81400affcbf75645866975a669e9
Carson Katri <Carson.katri@gmail.com>
2018-01-28 20:39:30 -0500

    Fix 500 error (maybe)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var Nuance = require('nuance');
-
 var path = require('path');
-var fs = require('fs');
-var logger = require('heroku-logger');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
